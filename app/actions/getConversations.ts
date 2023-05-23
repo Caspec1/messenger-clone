3c92f0255f64f78a5cb53a2d3d8c162d2c874586
Javier Miranda <mirandavillegasjavier@gmail.com>
2023-05-23 13:54:52 -0400

    conversations & messages

---
+++ app/actions/getConversations.ts
@@ -0,0 +1,36 @@
+import prisma from '@/app/libs/prismadb';
+import getCurrentUser from './getCurrentUser';
+
+const getConversations = async () => {
+  const currenUser = await getCurrentUser();
+
+  if (!currenUser?.id) return [];
+
+  try {
+    const conversations = await prisma.conversation.findMany({
+      orderBy: {
+        lastMessageAt: 'desc'
+      },
+      where: {
+        userIds: {
+          has: currenUser.id
+        }
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            sender: true,
+            seen: true
+          }
+        }
+      }
+    });
+    return conversations;
+  } catch (error: any) {
+    console.log(error);
+    return [];
+  }
+}
+
+export default getConversations
